4d99d2c697a00d63b6e30ed6cfc0c956af7de9aa
Shinyi Chen <jchen@isi.edu>
2017-03-23 14:42:45 -0700

    Update test with correct port

--- src/cserver/test.py
+++ src/cserver/test.py
@@ -2,7 +2,7 @@ import urllib2
 import requests
 
 print("initialize...")
-print(urllib2.urlopen("http://localhost:18080/initialize").read())
+print(urllib2.urlopen("http://localhost:8080/initialize").read())
 
 print("create template...")
 # payload = [{'image_path':'/nfs/div2/jchen/janus-dev/janus-api/src/cserver/test_imgs/example1.png', 'face_x': 39, 'face_y': 24, 'face_width': 175, 'face_height': 204},
@@ -11,7 +11,7 @@ print("create template...")
 payload = [{'image_path':"/lfs2/glaive/data/CS3_2.0/frames/10003.png", 'face_x': 638, 'face_y': 82, 'face_width': 185, 'face_height': 212},
     {'image_path':"/lfs2/glaive/data/CS3_2.0/frames/10004.png", 'face_x': 627, 'face_y': 93, 'face_width': 170, 'face_height': 206},
     {'image_path':"/lfs2/glaive/data/CS3_2.0/frames/10005.png", 'face_x': 597, 'face_y': 134, 'face_width': 175, 'face_height': 201}]
-r = requests.post("http://localhost:18080/search", json=payload)
+r = requests.post("http://localhost:8080/search", json=payload)
 print(r.status_code)
 print(r.text)
 
@@ -19,4 +19,4 @@ print(r.text)
 # print(result[0]['template_id'])
 
 print("finalize...")
-print(urllib2.urlopen("http://localhost:18080/finalize").read())
+print(urllib2.urlopen("http://localhost:8080/finalize").read())
